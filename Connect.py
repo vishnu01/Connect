a5bc3e65b37aa62d51c849d5a622281c86ecd559
vishnu shashank <shashankmv01@gmail.com>
2016-10-12 16:50:32 +0530

    Update Connect.py

--- Connect.py
+++ Connect.py
@@ -1,10 +1,27 @@
+import socket
 import os
-import webbrowser
-import subprocess as s
-ip=raw_input("Enter the IP/Domain name :")
-if(s.call(["ping",ip])==0):
-    print "valid internet" 
-    webbrowser.open("C:\Users\Public\Music\Sample Music\sleep.mp3")
-else :
-    print "no internet"
-    raw_input()
+def myplayer(text):
+    import pyaudio
+    import wave
+    chunk = 1024
+    f = wave.open(text)
+    p = pyaudio.PyAudio()
+    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
+                channels = f.getnchannels(),
+                rate = f.getframerate(),
+                output = True)
+    data = f.readframes(chunk)
+    while data != '':
+        stream.write(data)
+        data = f.readframes(chunk)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    return
+myip=socket.gethostbyname(socket.gethostname())
+if myip is "127.0.0.1":
+    print("You are not connected to the internet!")
+    myplayer("C:\Program Files\noint.wav")
+else:
+    print("You are connected to the internet!")
+    myplayer("C:\Program Files\int.wav")
